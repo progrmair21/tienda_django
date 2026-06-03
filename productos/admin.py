176b607f160f25b09582f6c55ffda93c42ce0017
Andres del Carpio <andres_vicoy_21@hotmail.com>
2026-06-03 03:09:14 -0300

    Mejora admin y agrega tests unitarios

--- productos/admin.py
+++ productos/admin.py
@@ -4,4 +4,7 @@ from .models import Producto
 @admin.register(Producto)
 class ProductoAdmin(admin.ModelAdmin):
     list_display = ('nombre', 'precio', 'stock', 'creado_en')
-    search_fields = ('nombre',)
+    search_fields = ('nombre', 'descripcion')
+    list_filter = ('creado_en',)
+    ordering = ('-creado_en',)
+    readonly_fields = ('creado_en',)
--- productos/tests.py
+++ productos/tests.py
@@ -1,3 +1,34 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from .models import Producto
 
-# Create your tests here.
+class ProductoTests(TestCase):
+    
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='test', 
+            password='test1234'
+        )
+        self.producto = Producto.objects.create(
+            nombre='Producto Test',
+            descripcion='Descripción test',
+            precio=100.00,
+            stock=10
+        )
+    
+    def test_lista_productos(self):
+        response = self.client.get('/productos/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_crear_producto_sin_login(self):
+        response = self.client.get('/productos/crear/')
+        self.assertEqual(response.status_code, 302)
+    
+    def test_crear_producto_con_login(self):
+        self.client.login(username='test', password='test1234')
+        response = self.client.get('/productos/crear/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_detalle_producto(self):
+        response = self.client.get(f'/productos/{self.producto.pk}/')
+        self.assertEqual(response.status_code, 200)
